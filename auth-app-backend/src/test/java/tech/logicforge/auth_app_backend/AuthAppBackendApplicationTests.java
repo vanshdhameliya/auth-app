fe0cc50ba0b2ac5ee750c37954619a599cc93fbb
vanshdhameliya <dhameliyavansh80001@gmail.com>
2026-09-19 16:59:09 +0530

    inital commit

---
+++ auth-app-backend/src/test/java/tech/logicforge/auth_app_backend/AuthAppBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.logicforge.auth_app_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthAppBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
